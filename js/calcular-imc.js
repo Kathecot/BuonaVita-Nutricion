af0c539d0d63e76bc37b04d570eae2b18c68c551
Katherine Cornejo <kathe.cornejo@gmail.com>
2022-06-27 16:03:03 -0500

    add new patient

---
+++ js/calcular-imc.js
@@ -0,0 +1,35 @@
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdIMC = paciente.querySelector(".info-imc");
+
+    pesoEsValido = true;
+    alturaEsValida = true;
+
+    if ((peso < 0) || (peso > 600)) {
+        console.log("Peso incorrecto");
+        tdIMC.textContent = "Peso incorrecto";
+        pesoEsValido = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if ((altura < 0) || (altura > 2.5)) {
+        console.log("Altura incorrecta");
+        tdIMC.textContent = "Altura incorrecta";
+        alturaEsValida = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if (pesoEsValido && alturaEsValida) {
+        var imc = peso / (altura * altura);
+        tdIMC.textContent = imc.toFixed(2);
+    }
+
+}
